4f6916516072d7b84cdc6b81cdab36eede54710d
LeeWonwoong <liweon09@gmail.com>
2026-06-16 22:16:56 +0900

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for N in 4 5 6; do
+    for Q in 1e-2 5e-3 1e-3; do
+        for A in 0.1 0.3 0.5 0.7 0.9; do
+            for R in 1.0 2.0 3.0; do
+
+                echo "=================================================="
+                echo "N_horizon=${N} Q_std=${Q} alpha=${A} R_std=${R}"
+                echo "=================================================="
+
+                python rhukf.py \
+                    --N_horizon ${N} \
+                    --alpha ${A} \
+                    --q_init ${Q} \
+                    --q_end ${Q} \
+                    --r_init ${R} \
+                    --r_end ${R}
+
+            done
+        done
+    done
+done
